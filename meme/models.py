7911079d95fbc0d06a16b8e2aef4a5cabd91c81d
Dario Castañé <i@dario.im>
2014-01-05 03:43:42 +0100

    Model done.

--- meme/models.py
+++ meme/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from bandera import settings
 
-# Create your models here.
+def build_upload_path(instance, filename):
+	return '/'.join(['ids', instance.token, filename])
+
+class Supporter(models.Model):
+	name = models.CharField(max_length=140)
+	email = models.EmailField()
+	region = models.CharField(max_length=2)
+	ok_candidate = models.BooleanField()
+	ok_tos = models.BooleanField()
+	token = models.CharField(max_length=64)
+	csrf = models.CharField(max_length=64)
+	scanned_id = models.FileField(upload_to=build_upload_path)
+
+	def __unicode__(self):
+		return '%s (%s)' % (self.name, self.email)
+
+class Candidate(models.Model):
+	supporter = models.ForeignKey(Supporter)
+	twitter = models.CharField(max_length=20)
+	facebook = models.URLField()
+	website = models.URLField()
+	bio = models.TextField()
+	phase = models.PositiveSmallIntegerField()
+	photo = models.FileField(upload_to=build_upload_path)
+
+	def __unicode__(self):
+		return '%s (%s)' % (self.supporter.name, self.supporter.email)
